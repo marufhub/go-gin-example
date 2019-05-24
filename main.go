1853a7213e8579dbec860e60401d7fd66a502925
Abdullah Al Maruf <eng.maruf@gmail.com>
2019-05-24 16:46:31 +0900

    add authentication middleware

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/marufhub/go-gin-example/middleware"
 	"github.com/marufhub/go-gin-example/models"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./view/build", true)))
-	v1 := r.Group("/api/users")
+	api := r.Group("/api/users")
 	{
-		v1.GET("/", models.GetUsers)
-		v1.GET("/:user_id", models.GetUser)
+		api.GET("/", models.GetUsers)
+		api.GET("/:user_id", models.GetUser)
+	}
+	// basic authentication endpoints
+	{
+		basicAuth := api.Group("/admin")
+		basicAuth.Use(middleware.AuthenticationRequired("admin"))
+		{
+			basicAuth.GET("/", models.GetUsers)
+		}
 	}
 	r.Run(":5000")
 }
